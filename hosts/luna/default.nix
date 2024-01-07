0e43762beeb5332eb2873415d14c2ae71e4313b1
Sanmoji <me@smj.im>
2024-01-07 21:40:51 +0800

    chore: split user config

--- hosts/luna/default.nix
+++ hosts/luna/default.nix
@@ -22,6 +22,8 @@
     ../common/global
     ../common/optional/pipewire.nix
 
+    ../common/users/smj
+
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
   ];
@@ -78,29 +80,6 @@
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  users.users = {
-    # FIXME: Replace with your username
-    smj = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "password";
-      isNormalUser = true;
-      # openssh.authorizedKeys.keys = [
-      #   # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      # ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      description = "Sanmoji";
-      extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [
-        firefox
-        vscodium
-      #  thunderbird
-      ];
-    };
-  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
