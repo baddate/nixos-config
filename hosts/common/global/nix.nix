482343d14952c180df23ce17e45b0059da99f91d
Sanmoji <me@smj.im>
2024-01-07 20:43:05 +0800

    chore: split & add more nix config
    add gc control

---
+++ hosts/common/global/nix.nix
@@ -0,0 +1,40 @@
+{ inputs, lib, pkgs, ... }:
+{
+  nix = {
+    settings = {
+      # substituters = [
+      #   # cache mirror located in China
+      #   # status: https://mirror.sjtu.edu.cn/
+      #   "https://mirror.sjtu.edu.cn/nix-channels/store"
+      #   # status: https://mirrors.ustc.edu.cn/status/
+      #   # "https://mirrors.ustc.edu.cn/nix-channels/store"
+
+      #   "https://nix-community.cachix.org"
+      # ];
+      # trusted-public-keys = [
+      #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # ];
+      trusted-users = [ "root" "@wheel" ];
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      # warn-dirty = false;
+      # system-features = [ "kvm" "big-parallel" "nixos-test" ];
+      # flake-registry = ""; # Disable global flake registry
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Keep the last 3 generations
+      # options = "--delete-older-than +3";
+      options = "--delete-older-than 7d";
+    };
+
+    # Add each flake input as a registry
+    # To make nix3 commands consistent with the flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # Add nixpkgs input to NIX_PATH
+    # This lets nix2 commands still use <nixpkgs>
+    nixPath = [ "nixpkgs=${inputs.nixpkgs.outPath}" ];
+  };
+}
