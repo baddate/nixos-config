28136f8021d41eb734fd7dca8b28ae395a6f5903
Sanmoji <me@smj.im>
2024-01-08 20:20:36 +0800

    fix: use builtin eza config

--- home/smj/cli/default.nix
+++ home/smj/cli/default.nix
@@ -7,11 +7,14 @@
     bottom # System viewer
     btop # System viewer
     ncdu # TUI disk usage
-    eza # Better ls
     ripgrep # Better grep
     fd # Better find
     jq # JSON pretty printer and manipulator
     timer # To help with my ADHD paralysis
     neofetch # Machine info viewer
   ];
+  programs.eza = {
+    enable = true;
+    enableAliases = true;
+  };
 }
