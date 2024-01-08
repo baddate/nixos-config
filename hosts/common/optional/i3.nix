7adedf5abb262efd6cd5e6379140a9e4f0abd1dc
Sanmoji <me@smj.im>
2024-01-08 12:33:25 +0800

    feat: add i3 pkgs and change to i3 & lightdm

---
+++ hosts/common/optional/i3.nix
@@ -0,0 +1,30 @@
+{pkgs, ...}: {
+  services = {
+    xserver = {
+      enable = true;
+      layout = "us";
+      xkbVariant = "";
+      desktopManager.runXdgAutostartIfNone = true;
+      displayManager.lightdm = {
+        enable = true;
+      };
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          i3            # i3
+          i3blocks      # status bar
+          i3lock        # default i3 screen locker
+          i3status      # provide information to i3bar
+          rofi          # application launcher, the same as dmenu
+          dunst         # notification daemon
+          xautolock     # lock screen after some time
+          picom         # transparency and shadows
+          feh           # set wallpaper
+          xbindkeys     # bind keys to commands
+          xorg.xdpyinfo # get screen information
+          sysstat       # get system information
+      ];
+      };
+    };
+  };
+}
