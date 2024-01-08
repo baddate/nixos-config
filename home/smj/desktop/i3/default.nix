353564818303fde414139efc297d7a29d24bf888
Sanmoji <me@smj.im>
2024-01-08 15:57:11 +0800

    fix: config flameshot

--- home/smj/desktop/i3/default.nix
+++ home/smj/desktop/i3/default.nix
@@ -5,5 +5,11 @@
   home.file.".config/i3/config".source = ./config;
   services.flameshot = {
     enable = true;
+    settings = {
+      General = {
+        disabledTrayIcon = true;
+        showStartupLaunchMessage = false;
+      };
+    };
   };
 }
