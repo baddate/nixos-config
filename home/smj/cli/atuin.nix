6f698efc8b690f554be7f0aa44b0a2dbf8f10a35
Sanmoji <me@smj.im>
2024-01-08 15:04:54 +0800

    fix: atuin: disable up arrow

--- home/smj/cli/atuin.nix
+++ home/smj/cli/atuin.nix
@@ -1,11 +1,17 @@
 {
   programs.atuin = {
     enable = true;
+    flags = [
+      "--disable-up-arrow"
+      # "--disable-ctrl-r"
+    ];
     settings = {
       auto_sync = true;
       sync_frequency = "10m";
       sync_address = "https://atuin.smj.im";
       search_mode = "prefix";
+      common_prefix = ["sudo"];
+      enter_accept = true;
     };
   };
 }
