6cf8399693892c7dd48d36063ba5c42164d7c957
Sanmoji <me@smj.im>
2024-01-09 09:41:54 +0800

    fix: using file-roller instead of xarchiver xarchiver was not working with thunar

--- hosts/common/optional/thunar.nix
+++ hosts/common/optional/thunar.nix
@@ -9,4 +9,5 @@
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
   programs.xfconf.enable = true; # enable save thunar config https://nixos.wiki/wiki/Thunar#Configuration
+  programs.file-roller.enable = true;
 }
