8f7e71a18d00ca6ff844a14cbb7dd7c282b58a23
Sanmoji <me@smj.im>
2024-01-09 18:01:58 +0800

    feat: add nixvim plugins
        ./autopairs
        ./comments
        ./core
        ./lsp
        ./telescope

--- home/smj/editor/neovim/plugins/default.nix
+++ home/smj/editor/neovim/plugins/default.nix
@@ -1,5 +1,9 @@
 {
   imports = [
-
+    ./autopairs
+    ./comments
+    ./core
+    ./lsp
+    ./telescope
   ];
 }
---
+++ home/smj/editor/neovim/plugins/lsp/lsp.nix
@@ -0,0 +1,30 @@
+{
+  programs.nixvim = {
+    plugins = {
+      lsp = {
+        enable = true;
+        servers = {
+          kotlin-language-server.enable = true;
+          lua-ls.enable = true;
+          nil_ls.enable = true;
+          marksman.enable = true;
+          pylsp.enable = true;
+          yamlls.enable = true;
+        };
+      };
+      lspkind = {
+        enable = true;
+      };
+      rust-tools = {
+        enable = true;
+        server.check.command = "clippy";
+      };
+      nvim-jdtls = {
+        enable = true;
+        # sneak into `.idea` project folder
+        data = ".idea/nvim-jdtls";
+      };
+      none-ls.enable = true;
+    };
+  };
+}
