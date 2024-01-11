bd430839409fc7c61707ebd15b43b2e37dac072f
Sanmoji <me@smj.im>
2024-01-11 21:58:41 +0800

    chore: change how pkgs import

--- flake.nix
+++ flake.nix
@@ -60,7 +60,8 @@
   in {
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    # packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    packages = forEachSystem (pkgs: import ./pkgs { inherit pkgs; });
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
