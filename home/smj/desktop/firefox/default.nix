6bb613e60f0dc25c233fcf26cda74665054c8fc3
Sanmoji <me@smj.im>
2024-01-13 14:55:52 +0800

    fix: add more firefox custom config & extensions

--- home/smj/desktop/firefox/default.nix
+++ home/smj/desktop/firefox/default.nix
@@ -39,9 +39,18 @@
         SearchEngines = {
           Default = "DuckDuckGo";
         };
+        FirefoxHome = {
+          Search = true;
+          TopSites = false;
+          SponsoredTopSites = false;
+          Highlights = false;
+          Pocket = false;
+          SponsoredPocket = false;
+          Snippets = true;
+        };
 
-        /* ---- EXTENSIONS ---- */
-        # Check about:support for extension/add-on ID strings.
+        /* ---- extensions ---- */
+        # check about:support for extension/add-on ID strings.
         # Valid strings for installation_mode are "allowed", "blocked",
         # "force_installed" and "normal_installed".
         ExtensionSettings = {
@@ -56,10 +65,25 @@
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
             installation_mode = "force_installed";
           };
+          # I still don't care about cookies:
+          "idcac-pub@guus.ninja" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/istilldontcareaboutcookies/latest.xpi";
+            installation_mode = "normal_installed";
+          };
+          # Clearurls:
+          "{74145f27-f039-47ce-a470-a662b129930a}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
+            installation_mode = "normal_installed";
+          };
+          # Bitwarden:
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "normal_installed";
+          };
           # Immersive Translate:
           "{5efceaa7-f3a2-4e59-a54b-85319448e305}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/immersive-translate/latest.xpi";
-            installation_mode = "normal_installed";
+            installation_mode = "allowed";
           };
         };
   
@@ -67,7 +91,7 @@
         # Check about:config for options.
         Preferences = { 
           "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-          "extensions.pocket.enabled" = lock-false;
+          # "extensions.pocket.enabled" = lock-false;
           "extensions.screenshots.disabled" = lock-true;
           "browser.topsites.contile.enabled" = lock-false;
           "browser.formfill.enable" = lock-false;
@@ -77,13 +101,58 @@
           "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
           "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
           "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
-          "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+          # "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
           "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
           "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
           "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
           "browser.newtabpage.activity-stream.showSponsored" = lock-false;
           "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+          
+          # myself custome
+          # Show "Not Secure" for insecure sites
+          "security.insecure_connection_text.enabled" = lock-true;
+          "security.insecure_connection_text.pbmode.enabled" = lock-true;
+
+          # Enable HTTPS-only mode
+          "dom.security.https_only_mode" = lock-true;
+          "dom.security.https_only_mode_ever_enabled" = lock-true;
+          
+          # https://shen.hong.io/nixos-for-philosophy-installing-firefox-latex-vscodium/
+          # Privacy settings
+          "privacy.donottrackheader.enabled" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "privacy.trackingprotection.socialtracking.enabled" = true;
+          "privacy.partition.network_state.ocsp_cache" = true;
+
+          # Disable all sorts of telemetry
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+          
+          # As well as Firefox 'experiments'
+          "experiments.activeExperiment" = false;
+          "experiments.enabled" = false;
+          "experiments.supported" = false;
+          "network.allow-experiments" = false;
+
+          # Disable Pocket Integration
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+          "extensions.pocket.enabled" = false;
+          "extensions.pocket.api" = "";
+          "extensions.pocket.oAuthConsumerKey" = "";
+          "extensions.pocket.showHome" = false;
+          "extensions.pocket.site" = "";
         };
       };
     };
