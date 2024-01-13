40cc45e62479617a0a8700c4cd042607ee49a4c2
Sanmoji <me@smj.im>
2024-01-13 10:08:14 +0800

    fix: add more plugins

--- home/smj/editor/astronvim/custom/init.lua
+++ home/smj/editor/astronvim/custom/init.lua
@@ -7,7 +7,6 @@ return {
       signcolumn = "auto", -- Show sign column when used only
       spell = false,      -- Spell checking
       swapfile = false,   -- Swapfile
-      smartindent = false, -- fix https://github.com/ryan4yin/nix-config/issues/4
       title = true,       -- Set the title of window to `filename [+=-] (path) - NVIM`
       -- The percentage of 'columns' to use for the title
       -- When the title is longer, only the end of the path name is shown.
@@ -25,6 +24,32 @@ return {
       opts = function(_, opts)
         opts.flavour = "macchiato"
         opts.transparent_background = true -- setting the background color.
+      end,
+    },
+    -- Motion
+    { import = "astrocommunity.motion.mini-surround" },
+    -- https://github.com/echasnovski/mini.ai
+    { import = "astrocommunity.motion.mini-ai" },
+    { import = "astrocommunity.motion.flash-nvim" },
+    -- diable toggleterm.nvim, zellij's terminal is far better than neovim's one
+    -- { "akinsho/toggleterm.nvim",                                   enabled = false },
+    { "folke/flash.nvim",                                          vscode = false },
+    -- Highly experimental plugin that completely replaces
+    -- the UI for messages, cmdline and the popupmenu.
+    { import = "astrocommunity.utility.noice-nvim" },
+
+    { import = "astrocommunity.media.vim-wakatime" },
+    { import = "astrocommunity.motion.leap-nvim" },
+    { import = "astrocommunity.motion.flit-nvim" },
+    { import = "astrocommunity.scrolling.nvim-scrollbar" },
+    { import = "astrocommunity.editing-support.todo-comments-nvim" },
+
+    {
+      "0x00-ketsu/autosave.nvim",
+      -- lazy-loading on events
+      event = { "InsertLeave", "TextChanged" },
+      opts = function(_, opts)
+        opts.prompt_style = "notify" -- notify or stdout
       end,
     },
   },
