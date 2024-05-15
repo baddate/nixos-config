30d765827e7b82a50d4f77945d0ddf5ead70a893
Sanmoji <me@smj.im>
2024-05-15 11:53:20 +0800

    fix: update config causing osprober

--- hosts/common/optional/grub.nix
+++ hosts/common/optional/grub.nix
@@ -5,7 +5,15 @@
       device = "nodev";
       efiSupport = true;
       enableCryptodisk = true;
-      useOSProber = true;
+      useOSProber = false;
+      extraEntries = ''
+      menuentry 'Windows' --class windows --class os $menuentry_id_option 'osprober-efi-8A74-2073' {
+        insmod part_gpt
+        insmod fat
+        search --no-floppy --fs-uuid --set=root 8A74-2073
+        chainloader /efi/Microsoft/Boot/bootmgfw.efi
+      }
+      '';
     };
     efi.canTouchEfiVariables = true;
   };
